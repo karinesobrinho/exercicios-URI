adbb50d9db02c4e06e262b7e6f76cacd7ef47102
Karine Sobrinho <karine.silva.sobrinho@gmail.com>
2020-08-10 12:19:58 -0300

    1060

---
+++  1060-NumerosPositivos.py
@@ -0,0 +1,28 @@
+p = 0
+
+a = float(input())
+b = float(input())
+c = float(input())
+d = float(input())
+e = float(input())
+f = float(input())
+
+if a>0:
+  p = p+1
+
+if b>0:
+  p = p+1
+
+if c>0:
+  p = p+1
+
+if d>0:
+  p = p+1
+
+if e>0:
+  p = p+1
+
+if f>0:
+  p = p+1
+
+print(p, "valores positivos")
