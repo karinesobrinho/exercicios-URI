bc84867347e224eb697d1f17622aefe9cadb277c
Karine Sobrinho <karine.silva.sobrinho@gmail.com>
2020-08-10 12:03:58 -0300

    1184

---
+++  1184-AbaixodaDiagonalPrincipal.py
@@ -0,0 +1,22 @@
+M = [[0 for i in range (12)] for j in range (12)]
+
+O = input()
+R = 0
+
+for i in range (12):
+  for j in range (12):
+    M[i][j] = float(input())
+
+if O == "S":
+  for i in range (len(M)):
+    for j in range (i+1, len(M[0])):
+      R += M[j][i]
+            
+
+else:
+  for i in range (len(M)):
+    for j in range (i+1, len(M[0])):
+      R += (M[j][i])/66
+    
+
+print ("%.1f"%R)
