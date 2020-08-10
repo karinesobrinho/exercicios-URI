c970ca86e72f59805ff6e5fc658aa834ce257e45
Karine Sobrinho <karine.silva.sobrinho@gmail.com>
2020-08-10 12:19:12 -0300

    1134

---
+++  1134-TipodeCombustivel.py
@@ -0,0 +1,20 @@
+a = 0
+g = 0
+d = 0
+
+n=int(input())
+while n!=4:
+    n=int(input())
+    if n==1:
+      a = a+1
+
+    if n==2:
+      g = g+1
+
+    if n==3:
+      d= d+1
+
+print("MUITO OBRIGADO")
+print("Alcool:", a)
+print("Gasolina:", g)
+print("Diesel:", d)
