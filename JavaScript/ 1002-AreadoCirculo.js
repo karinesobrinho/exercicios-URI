5238eb6586ac4e022c7f4349b771a3891e7a47b1
karine sobrinho <karine.silva.sobrinho@gmail.com>
2021-04-21 16:56:21 -0300

    ok

--- JavaScript/ 1002-AreadoCirculo.js
+++
@@ -1,6 +0,0 @@
-var input = require('fs').readFileSync('/dev/stdin', 'utf8');
-var lines = input.split('\n');
-
-var r = parseFloat(lines.shift())
-
-console.log("A=" + (3.14159 * Math.pow(r,2)).toFixed(4))
